474ee2daa32c41410a2232282646e3046bd60273
ii-shimul <islamshimul27@gmail.com>
2025-03-29 20:50:51 +0600

    added pagination functionality

--- src/pages/UsersList/UsersList.jsx
+++ src/pages/UsersList/UsersList.jsx
@@ -3,23 +3,40 @@ import { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
 const UsersList = () => {
-	const [users, setUsers] = useState([]);
+	const [users, setUsers] = useState({ data: [], total: 0 });
+	const [page, setPage] = useState(1);
 
 	useEffect(() => {
 		const fetchUsers = async () => {
-			const result = await axios.get("https://reqres.in/api/users");
-			setUsers(result.data);
+			try {
+				const result = await axios.get(
+					`https://reqres.in/api/users?page=${page}`
+				);
+				setUsers(result.data);
+			} catch (error) {
+				console.error("Error fetching users:", error);
+			}
 		};
 		fetchUsers();
-	}, []);
+	}, [page]);
 
-	const handleEdit = () => {};
+	if (!users.data.length) return <p className="text-center text-3xl">Loading...</p>;
+
+	const handleEdit = (row) => {
+		console.log("Editing user:", row);
+	};
+
+	const handlePageChange = (newPage) => {
+		setPage(newPage);
+	};
 
 	const columns = [
-    {
-      name: "Image",
-      selector: (row) => <img src={row.avatar} alt="" className="w-16 rounded-full my-1" />
-    },
+		{
+			name: "Image",
+			selector: (row) => (
+				<img src={row.avatar} alt="" className="w-16 rounded-full my-1" />
+			),
+		},
 		{
 			name: "Name",
 			selector: (row) => `${row.first_name} ${row.last_name}`,
@@ -31,9 +48,19 @@ const UsersList = () => {
 			cell: (row) => <button onClick={() => handleEdit(row)}>Edit</button>,
 		},
 	];
+
 	return (
 		<div className="w-full">
-			<DataTable columns={columns} data={users.data} title="Users" />
+			<DataTable
+				columns={columns}
+				data={users.data}
+				title="Users"
+				pagination
+				paginationServer
+				paginationPerPage={6}
+				paginationTotalRows={users.total}
+				onChangePage={handlePageChange}
+			/>
 		</div>
 	);
 };
